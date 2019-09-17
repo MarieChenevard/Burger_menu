21f4ea91d12ccbd08758a8328872ad4958f8c958
MarieChenevard <marie.chenevard@gmail.com>
2019-09-17 09:35:26 +0200

    upload

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 var btn = document.querySelector('.toggleBtn');
-var nav = document.querySelector('header');
+var nav = document.querySelector('.nav');
 
 btn.onclick = function(){
     nav.classList.toggle('nav_open');
 
 }
+
+btn.addEventListener('click',()=>{
+    btn.classList.toggle('active');
+});
